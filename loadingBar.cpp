b4e10e42c006759af65b9d54a8f9bb65a880b706
mariaem21 <mematamoros@icloud.com>
2023-09-29 13:45:10 -0500

    Wrote function for loadingBar

--- loadingBar.cpp
+++ loadingBar.cpp
@@ -1,3 +1,22 @@
-int loadingBar() {
-    return 0;
+#include <cstdlib> 
+#include <iostream>
+#include <unistd.h>
+#include "doSomething.cpp"
+
+void loadingBar(void) {
+    int intVar;
+
+    system("clear");
+    doSomething();
+    std::cout << "\n\n\n\n\n\t\t\t\t\tStudent\'s Data Management Syste m";
+    std::cout << "\n\t\t\t\t\tProject By Ajay Singh";
+    std::cout << "\n\t\t\t\t\tForked by Table 11 for CSCE 451, Educational purposes";
+    std::cout << "\n\n\n\t\t\t\t\tLoading...\n";
+    std::cout << std::endl;
+    std::cout << "\t\t\t\t\t";
+
+    for (intVar = 0; intVar < 5; intVar +=1) {
+        sleep(1);
+    }
+    return;
 }
