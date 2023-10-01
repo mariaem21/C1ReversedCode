5b06c5d5832917d87d01aad8f0f2ba3cce42176b
BrianWinner <brianwin1235@gmail.com>
2023-10-01 17:17:45 -0500

    Brian first decompile

---
+++ test3.h
@@ -0,0 +1,7 @@
+#ifndef TEST3_H
+#define TEST3_H
+#include <string>
+
+void test2(char* input);
+
+#endif
---
+++ undo.h
@@ -0,0 +1,7 @@
+#ifndef UNDO_H
+#define UNDO_H
+#include <string>
+
+std::string* undo(std::string input);
+
+#endif
