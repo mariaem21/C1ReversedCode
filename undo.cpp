37d6760d00989eb1c2f456ab293a9f2a02b9824e
BrianWinner <brianwin1235@gmail.com>
2023-10-05 22:30:31 -0500

    Tyson's undo

--- undo.cpp
+++ undo.cpp
@@ -1,10 +1,13 @@
 #include <string>
-#include <iostream>
-#include <cstring>
+#include <sstream>
 
-std::string* undo(std::string input){
-    for (int i = 0; i < sizeof(input); i++) {
-        input.push_back(input[i]);
-    }
-    return &input;
+std::string* undo(std::string& raw) {
+  // raw: a b c d 
+  std::stringstream ss(raw);
+  std::string result;
+  char c;
+  while (ss >> c) {
+    result += c;
+  }
+  return &result;
 }
